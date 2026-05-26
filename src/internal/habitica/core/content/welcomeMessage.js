98cfdaf9ecd87737e9ab74887969da3cd1b71c9d
JDudzik <jdudzik950@gmail.com>
2026-05-26 16:42:46 -0400

    Fix capitalized letter

--- src/internal/habitica/core/content/welcomeMessage.js
+++ src/internal/habitica/core/content/welcomeMessage.js
@@ -7,5 +7,5 @@ Thanks for joining [HabiTools](${ habitoolsUrl }). I'm the official _(automated)
 
 If you haven't already, you can start activating tools that interest you.
 
-If you have any questions, suggestions, or bug reports, The fastest way to reach the developer of HabiTools is to use the [feedback page](${ habitoolsUrl }/feedback/).
+If you have any questions, suggestions, or bug reports, the fastest way to reach the developer of HabiTools is to use the [feedback page](${ habitoolsUrl }/feedback/).
 `;
