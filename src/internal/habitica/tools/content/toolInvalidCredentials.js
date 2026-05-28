22ab469605062909f2ed2fd7a32ad318d6ebae6e
JDudzik <jdudzik950@gmail.com>
2026-05-28 09:01:46 -0400

    Fix message newlines

--- src/internal/habitica/tools/content/toolInvalidCredentials.js
+++ src/internal/habitica/tools/content/toolInvalidCredentials.js
@@ -2,8 +2,8 @@ const habitoolsUrl = process.env.FRONTEND_HOST;
 
 export default toolName => `
 Looks like something went wrong on HabiTools. The ${ toolName } tool has encountered an error and is unable to continue running. 
-This tool has been deactivated as a precaution. **Please unlink and relink your Habitica account** 
-again to continue using it. You can do so from [your account page](${ habitoolsUrl }/my-account/).
+
+This tool has been deactivated as a precaution. **Please unlink and relink your Habitica account**  again to continue using it. You can do so from [your account page](${ habitoolsUrl }/my-account/).
 
 If this issue persists after relinking your account, please reach out. The [feedback page](${ habitoolsUrl }/feedback) is the quickest way for us to receive your message.
 `;
