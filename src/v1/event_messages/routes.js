9bd429fb6bdd40bae7e5e7e2f942fa73ee6da07d
JDudzik <jdudzik950@gmail.com>
2026-05-28 15:31:40 -0400

    Remove limit from list event-message API

--- src/v1/event_messages/routes.js
+++ src/v1/event_messages/routes.js
@@ -6,10 +6,9 @@ const event_messages = require('./event_messages');
 module.exports = (router) => {
   // const openPath = '/event-messages';
   const securedPath = '/auth/event-messages';
-  
+
   // Brute-force prevention
   bruteStopper(router, `${ securedPath }/create`, { freeRetries: 30 });
-  bruteStopper(router, `${ securedPath }/list`, { freeRetries: 100, minWait: 50, maxWait: 1500, lifetime: 50 });
   bruteStopper(router, `${ securedPath }/acknowledge`, { freeRetries: 30 });
   bruteStopper(router, `${ securedPath }/unacknowledge`, { freeRetries: 30 });
   bruteStopper(router, `${ securedPath }/delete/:message_id`, { freeRetries: 30 });
@@ -20,8 +19,6 @@ module.exports = (router) => {
   router.put(`${ securedPath }/acknowledge`, event_messages.acknowledge);
   router.put(`${ securedPath }/unacknowledge`, event_messages.unacknowledge);
   router.delete(`${ securedPath }/delete/:message_id`, event_messages.deleteMessage);
-  
-
 
   return router;
 };
